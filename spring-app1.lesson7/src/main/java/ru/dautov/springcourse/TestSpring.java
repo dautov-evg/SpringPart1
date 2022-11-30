8d7b86869841c41123779d9732e5650b9594e7f9
Evgeniy Dautov <evg.dautov@mail.ru>
2022-11-30 06:09:53 +0300

    Lesson 7 end

---
+++ spring-app1.lesson7/src/main/java/ru/dautov/springcourse/TestSpring.java
@@ -0,0 +1,27 @@
+package ru.dautov.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = firstMusicPlayer==secondMusicPlayer;
+        System.out.println(comparison);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+//        firstMusicPlayer.playMusic();
+
+        context.close();
+    }
+}
