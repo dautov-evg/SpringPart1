51940cdf2d6a7bb32e7980b728d6b3464d6deef4
Evgeniy Dautov <evg.dautov@mail.ru>
2022-11-30 03:40:08 +0300

    Lesson 5 end

---
+++ spring-app1.lesson5/src/main/java/ru/dautov/springcourse/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package ru.dautov.springcourse;
+
+public class ClassicalMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ spring-app1.lesson5/src/main/java/ru/dautov/springcourse/Music.java
@@ -0,0 +1,5 @@
+package ru.dautov.springcourse;
+
+public interface Music {
+    String getSong();
+}
