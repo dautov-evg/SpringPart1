8d8babe8a889e19960c431b10aae5ffcfa501d5b
Evgeniy Dautov <evg.dautov@mail.ru>
2022-11-30 05:13:49 +0300

    Lesson 6 end

---
+++ spring-app1.lesson6/src/main/java/ru/dautov/springcourse/TestSpring.java
@@ -0,0 +1,19 @@
+package ru.dautov.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+
+        musicPlayer.playMusic();
+
+
+        context.close();
+    }
+}
