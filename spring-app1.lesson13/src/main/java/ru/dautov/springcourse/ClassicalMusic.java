5407490159d567d9c856d0688e17525f3fcd342a
Evgeniy Dautov <evg.dautov@mail.ru>
2022-12-01 11:55:22 +0300

    Lesson 13

---
+++ spring-app1.lesson13/src/main/java/ru/dautov/springcourse/ClassicalMusic.java
@@ -0,0 +1,26 @@
+package ru.dautov.springcourse;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+
+public class ClassicalMusic implements Music{
+    
+
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Doing my initialisation");
+    }
+
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
+
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
